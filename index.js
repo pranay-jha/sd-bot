9ed59a6209e03a4cd99e154a8603d96102cdf47a
Pranay Jha <pranay.jha@gmail.com>
2020-07-22 01:21:21 -0700

    fixed setActivity

--- index.js
+++ index.js
@@ -18,10 +18,8 @@ client.registry
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setActivity('with Commando');
+    client.user.setActivity('the waiting game...');
 });
-
-client.user.setActivity("Waiting...")
 
 client.on('error', console.error);
     
